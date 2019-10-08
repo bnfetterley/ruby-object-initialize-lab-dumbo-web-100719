2eb800b368c8e1e4ea403abd1852c5c3ec28c3f2
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:28:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,8 @@
 class Dog
   
- def initialize(name)
+ def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
-  # def initialize(breed = mutt)
-  #   @breed = breed
-  # end
 end
